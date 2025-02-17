9ba5e1d02f7476e9ed03d2d074bbf68c25231a5b
codingwithowais <owaisahmad1399@gmail.com>
2025-02-18 01:11:38 +0530

    Variables file added

---
+++ 01_basics/02_variables.js
@@ -0,0 +1,15 @@
+const id = 125466;
+let city = "Lucknow";
+var state = "UP";
+
+city = "Kanpur";
+state = "MP";
+
+// We can not update id as it is constant
+
+// city and state can be updated but let and var differ
+
+// let is a block scope element whereas var is not
+
+// It is always a best practice to use const and let due to issue of var in 
+// block scope and functional scope

8f8114f750082fe054beac1c28fa7ed04a346fdb
codingwithowais <owaisahmad1399@gmail.com>
2025-02-18 21:35:59 +0530

    dataTypes file added

---
+++ 01_basics/03_dataTypes.js
@@ -0,0 +1,15 @@
+let id = 12561; // Numeric Data Type
+let name = "Owais"; // String Data Type
+let isEmployee = true; // Boolean Data Type
+let temp = null; // Null is a value its data type is object
+let location = undefined;  // undefined data type
+
+// value and data type is undefined if we do not assign any value to a variable
+
+console.log(typeof id);
+console.log(typeof name);
+console.log(typeof isEmployee);
+console.log(typeof temp);
+console.log(typeof location);
+
+

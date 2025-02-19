40a6937ec4f52a26289df3f66aa706d72d19abf9
codingwithowais <owaisahmad1399@gmail.com>
2025-02-20 00:58:17 +0530

    numbers and math file added

---
+++ 01_basics/08_numbers-math.js
@@ -0,0 +1,50 @@
+// Numbers 
+
+const score = new Number(33);
+// console.log(score);
+// console.log(score.toString());
+
+// const balance = 1234.1520;
+// console.log(balance.toFixed(2));
+// console.log(balance.toPrecision(5));
+// const amount = 10000000;
+// console.log(amount.toLocaleString());
+
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// Maths in js
+
+// Math is an inbuilt obj in js
+// console.log(Math);
+
+// Methods and properties of Math object
+// console.log(Math.PI); // get the value of pie
+// console.log(Math.sqrt(25.255)); 
+// console.log(Math.round(75.2635));
+// console.log(Math.ceil(4.1));
+// console.log(Math.floor(4.9));
+
+// console.log(Math.min(2 , 3, 5 , 10));
+// console.log(Math.max(2, 3 , 5 , 10));
+
+// console.log(Math.random()); // generates a random no. between [0 , 1]
+
+// formula to get a random number between [a , b]:
+
+// a + (b-a)*random_number
+
+let a = 20;
+let b  = 50;
+// console.log((a + Math.floor((b-a)*Math.random())));
+
+
+
+
+
+
+
+
+
+
+
+
